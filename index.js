1cc5e903676d7840a6a2983e82671ba08a0e381a
just ivel554 <justivel554@justs-MacBook-Pro.local>
2025-06-24 14:30:45 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+customerName = "bob";
+function upperCaseCustomerName(customerName) {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+function setBestCustomer() {
+   var bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  var bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+function LeastFavoriteCustomer() {
+  var const leastFavoriteCustomer = "susan";
+  return leastFavoriteCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+   var leastFavoriteCustomer = "sally";
+  return leastFavoriteCustomer;
+}
+
+
+
